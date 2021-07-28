0b80bb85f1b1e27e4442824342d5d0d8cca47c59
Guilherme <guilhermebehs2013@hotmail.com>
2021-07-28 17:36:35 -0300

    fix: sort grouped orders in code

--- src/infra/db/mongodb/get-grouped-orders-mongo-repository.ts
+++ src/infra/db/mongodb/get-grouped-orders-mongo-repository.ts
@@ -4,7 +4,7 @@ import { MongoHelper } from './utils/mongo-helper'
 export class GetGroupedOrdersMongoRepository implements GetGroupedOrdersRepository {
   async get (): Promise<GroupedOrdersModel[]> {
     const collection = await MongoHelper.getCollection('order')
-    const orders = await collection.aggregate([
+    const groupedOrders: GroupedOrdersModel[] = await collection.aggregate([
       {
         $group:
          {
@@ -13,16 +13,24 @@ export class GetGroupedOrdersMongoRepository implements GetGroupedOrdersReposito
          }
       },
       {
-        $sort: { wonTime: -1 }
-      },
-      {
         $project: {
           _id: 0,
           data: '$_id.wonTime',
           valorTotal: '$totalValue'
         }
       }
-    ])
-    return await orders.toArray()
+    ]).toArray()
+    const sortedAndGroupedOrders = this.sortGroupedOrders(groupedOrders)
+    return sortedAndGroupedOrders
+  }
+
+  private sortGroupedOrders (groupedOrders: GroupedOrdersModel[]): GroupedOrdersModel[] {
+    const sortedAndGroupedOrders = groupedOrders.sort((group1, group2) => {
+      const group1Date = new Date(group1.data).getTime()
+      const group2Date = new Date(group2.data).getTime()
+      if (group1Date > group2Date) { return -1 }
+      return 1
+    }) as unknown as GroupedOrdersModel[]
+    return sortedAndGroupedOrders
   }
 }
