8497ef86622e55c07893661eec7a134740768ee5
Guilherme <guilhermebehs2013@hotmail.com>
2021-07-28 08:28:39 -0300

    feat: ensure InsertOrderMongoRepository inserts and returnas correct data

---
+++ src/infra/db/mongodb/insert-order-mongo-repository.ts
@@ -0,0 +1,11 @@
+import { MongoHelper } from './utils/mongo-helper'
+import { OrderModel } from '../../../domain/models/order'
+import { InsertOrderRepository } from './../../../implementation/protocols/insert-order-repository'
+export class InsertOrderMongoRepository implements InsertOrderRepository {
+  async insert (order: OrderModel): Promise<OrderModel> {
+    const collection = await MongoHelper.getCollection('order')
+    await collection.insertOne(order)
+    const insertedOrder = await collection.findOne({ id: order.id })
+    return insertedOrder
+  }
+}
