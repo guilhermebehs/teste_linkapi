cc5d2c277dd5176646d151e7ef502e298c80c7da
Guilherme <guilhermebehs2013@hotmail.com>
2021-07-28 13:16:52 -0300

    test: ensure GetOrdersUseCase return all orders returned from GetOrdersRepository

--- src/implementation/get-orders.spec.ts
+++ src/implementation/get-orders.spec.ts
@@ -2,6 +2,13 @@ import { GetOrdersRepository } from './protocols/get-orders-repository'
 import { OrderModel } from './../domain/models/order'
 import { GetOrdersUseCases } from './get-orders'
 
+const data = [{
+  id: '1',
+  clientName: 'any_name',
+  salerName: 'any_name',
+  products: [],
+  wonTime: '2020-01-02'
+}]
 interface SutTypes {
   getOrdersRepositoryStub: GetOrdersRepository
   sut: GetOrdersUseCases
@@ -9,13 +16,7 @@ interface SutTypes {
 
 class GetOrdersRepositoryStub implements GetOrdersRepository {
   async get (): Promise<OrderModel[]> {
-    return [{
-      id: '1',
-      clientName: 'any_name',
-      salerName: 'any_name',
-      products: [],
-      wonTime: '2020-01-02'
-    }]
+    return data
   }
 }
 
@@ -35,5 +36,10 @@ describe('GetOrders UseCase', () => {
     jest.spyOn(getOrdersRepositoryStub, 'get').mockImplementationOnce(() => { throw new Error() })
     const promise = sut.get()
     await expect(promise).rejects.toThrow()
+  })
+  test('Should return all orders returned from GetOrdersRepository', async () => {
+    const { sut } = makeSut()
+    const returnedData = await sut.get()
+    await expect(returnedData).toEqual(data)
   })
 })
