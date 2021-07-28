ec3d2908332c610e08a615ebe28549eaca63c0a6
Guilherme <guilhermebehs2013@hotmail.com>
2021-07-28 08:28:48 -0300

    test: ensure InsertOrderMongoRepository inserts and returnas correct data

---
+++ src/infra/db/mongodb/insert-order-mongo-repository.spec.ts
@@ -0,0 +1,39 @@
+import { InsertOrderMongoRepository } from './insert-order-mongo-repository'
+import { MongoHelper } from './utils/mongo-helper'
+import { InsertOrderRepository } from '../../../implementation/protocols/insert-order-repository'
+interface SutTypes{
+  sut: InsertOrderRepository
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new InsertOrderMongoRepository()
+  return { sut }
+}
+
+describe('InsertOrderMongo Repository', () => {
+  beforeAll(async () => {
+    const mongoUrl = process.env.MONGO_URL ?? ''
+    await MongoHelper.connect(mongoUrl)
+  })
+  afterEach(async () => {
+    const orderCollection = await MongoHelper.getCollection('order')
+    await orderCollection.deleteMany({})
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  test('Should insert and return correct data', async () => {
+    const { sut } = makeSut()
+    const orderCollection = await MongoHelper.getCollection('order')
+    const insertedOrder = {
+      id: '1',
+      clientName: 'any_name',
+      salerName: 'any_name',
+      wonTime: '2020-02-01',
+      products: []
+    }
+    await sut.insert(insertedOrder)
+    const order = await orderCollection.findOne({ id: '1' })
+    expect(order).toEqual(insertedOrder)
+  })
+})
