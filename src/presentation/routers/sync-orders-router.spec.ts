09a8d698d061162d253cd1d6c1deb4b3b2b1c9ca
Guilherme <guilhermebehs2013@hotmail.com>
2021-07-29 11:18:28 -0300

    test: ensure SyncOrdersRouter returns 500 when SynchronizeOrders throws

---
+++ src/presentation/routers/sync-orders-router.spec.ts
@@ -0,0 +1,37 @@
+import { HttpResponse } from './../controllers/protocols/http-response'
+import { InternalError } from '../controllers/errors/internal-error'
+import { SynchronizeOrdersController } from '../controllers/sync-orders-controller'
+import { SyncOrdersRouter } from './sync-orders-router'
+import { SynchronizeOrders } from '../../domain/use-cases/synchronize-orders'
+
+interface SutTypes{
+  syncOrdersStub: SynchronizeOrders
+  syncOrdersControllerStub: SynchronizeOrdersController
+  sut: SyncOrdersRouter
+}
+
+const makeSyncOrdersStub = (): SynchronizeOrders => {
+  class SyncOrdersOrdersStub implements SynchronizeOrders {
+    async synchronize (): Promise<void> {
+    }
+  }
+  return new SyncOrdersOrdersStub()
+}
+
+const makeSut = (): SutTypes => {
+  const syncOrdersStub = makeSyncOrdersStub()
+  const syncOrdersControllerStub = new SynchronizeOrdersController(syncOrdersStub)
+  const sut = new SyncOrdersRouter(syncOrdersControllerStub)
+  return { sut, syncOrdersStub, syncOrdersControllerStub }
+}
+
+describe('SyncOrders Router', () => {
+  test('Should return 500 when SyncOrders throws', async () => {
+    const { sut, syncOrdersStub } = makeSut()
+    jest.spyOn(syncOrdersStub, 'synchronize').mockImplementationOnce(() => { throw new Error() })
+    const httpResponse: HttpResponse = await sut.route({})
+    expect(httpResponse).toBeTruthy()
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new InternalError())
+  })
+})
