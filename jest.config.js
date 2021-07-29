c691e3c057843f9d9438c0d6bf24690ca00883c2
Guilherme <guilhermebehs2013@hotmail.com>
2021-07-29 09:48:25 -0300

    refactor: remove index.ts and app.ts from jest coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!<rootDir>/src/main/app.ts',
+    '!<rootDir>/src/main/index.ts'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
